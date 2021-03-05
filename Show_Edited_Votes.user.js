1957556cd103ad52b52b5d3f46165ce0b6dbff7c
Nathan Tuggy <nathan@tuggycomputer.com>
2021-03-05 16:26:38 -0600

    Licensing info.

--- Show_Edited_Votes.user.js
+++ Show_Edited_Votes.user.js
@@ -6,6 +6,20 @@
 // @version     1.5.00
 // @grant       none
 // ==/UserScript==//
+/* Copyright Nathan Tuggy 2016-2019
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 // Throttling implementation borrowed from rene's Match Against Peers In Review
 
 function parseIsoDatetime(dtstr) {      // From http://stackoverflow.com/a/26434619
